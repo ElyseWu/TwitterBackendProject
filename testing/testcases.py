edd38e3862844d4e2a7da8395ecd6e4df62e848d
tong-meow <tongshen930@gmail.com>
2021-11-24 12:46:56 -0700

    Add comments model

--- testing/testcases.py
+++ testing/testcases.py
@@ -1,13 +1,22 @@
+from comments.models import Comment
 from django.contrib.auth.models import User
 from django.test import TestCase as DjangoTestCase
+from rest_framework.test import APIClient
 from tweets.models import Tweet
 
 
 class TestCase(DjangoTestCase):
 
+    @property
+    def anonymous_client(self):
+        if hasattr(self, '_anonymous_client'):
+            return self._anonymous_client
+        self._anonymous_client = APIClient()
+        return self._anonymous_client
+
     def create_user(self, username, email=None, password=None):
         if email is None:
-            email = f'{username}@twitter.com'
+            email = '{}@twitter.com'.format(username)
         if password is None:
             password = 'generic password'
         return User.objects.create_user(username, email, password)
@@ -16,3 +25,8 @@ class TestCase(DjangoTestCase):
         if content is None:
             content = 'default tweet content'
         return Tweet.objects.create(user=user, content=content)
+
+    def create_comment(self, user, tweet, content=None):
+        if content is None:
+            content = 'default comment content'
+        return Comment.objects.create(user=user, tweet=tweet, content=content)
