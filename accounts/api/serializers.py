19ff7a80c9b83ee2f33fef3624d90c7380419350
tong-meow <tongshen930@gmail.com>
2021-11-24 13:17:41 -0700

    Add comments api

--- accounts/api/serializers.py
+++ accounts/api/serializers.py
@@ -20,6 +20,12 @@ class UserSerializerForFriendship(serializers.ModelSerializer):
         fields = ('id', 'username')
 
 
+class UserSerializerForComment(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username')
+
+
 class SignupSerializer(serializers.ModelSerializer):
     username = serializers.CharField(max_length=20, min_length=6)
     password = serializers.CharField(max_length=20, min_length=6)
