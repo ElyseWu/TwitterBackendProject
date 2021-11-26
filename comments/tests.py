01f9fc15b55d58b6dda11a63a6641700d32191df
tong-meow <tongshen930@gmail.com>
2021-11-26 14:10:22 -0700

    Add likes model

--- comments/tests.py
+++ comments/tests.py
@@ -3,8 +3,21 @@ from testing.testcases import TestCase
 
 class CommentModelTests(TestCase):
 
+    def setUp(self):
+        self.jesse = self.create_user('jesse')
+        self.tweet = self.create_tweet(self.jesse)
+        self.comment = self.create_comment(self.jesse, self.tweet)
+
     def test_comment(self):
-        user = self.create_user('linghu')
-        tweet = self.create_tweet(user)
-        comment = self.create_comment(user, tweet)
-        self.assertNotEqual(comment.__str__(), None)
+        self.assertNotEqual(self.comment.__str__(), None)
+
+    def test_like_set(self):
+        self.create_like(self.jesse, self.comment)
+        self.assertEqual(self.comment.like_set.count(), 1)
+
+        self.create_like(self.jesse, self.comment)
+        self.assertEqual(self.comment.like_set.count(), 1)
+
+        eliza = self.create_user('eliza')
+        self.create_like(eliza, self.comment)
+        self.assertEqual(self.comment.like_set.count(), 2)
