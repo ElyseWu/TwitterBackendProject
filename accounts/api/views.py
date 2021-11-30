e50aa52d712cf462e874bea0da3445f69226bb66
tong-meow <tongshen930@gmail.com>
2021-11-29 17:03:23 -0700

    Add photo model, fix some bugs

--- accounts/api/views.py
+++ accounts/api/views.py
@@ -26,7 +26,7 @@ class UserViewSet(viewsets.ReadOnlyModelViewSet):
     """
     queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializerWithProfile
-    permission_classes = (permissions.IsAdminUser
+    permission_classes = (permissions.IsAdminUser,)
 
 class AccountViewSet(viewsets.ViewSet):
     permission_classes = (AllowAny,)
